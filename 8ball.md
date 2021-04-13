dd1bbc6d9c751c507ff7e182d87a3835281e9061
xSplode <>
2021-04-13 14:10:25 +0000

    Initial commit

---
+++ 8ball.md
@@ -0,0 +1,11 @@
+# Magic 8-Ball
+
+Simplistic Magic 8-Ball discord bot that replies with a randomized answer. It's very simple, even a 3-year-old can understand this. Maybe.
+
+# How it works
+
+Works exactly like AnswerBot, but slightly different. Using the "random" tool, the command will get a randomized answer and reply to the user with said answer. Told you it was simple
+
+# Add this bot
+
+You can add the bot here: https://discord.com/api/oauth2/authorize?client_id=831520530322554880&permissions=322560&scope=bot
---
+++ main.py
@@ -0,0 +1,39 @@
+# Magic 8 Ball Bot!
+# Made by Splode.
+
+import discord
+import random
+import os
+from keep_alive import keep_alive
+
+client = client = discord.Client()
+
+ball = [
+  "As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes, definitely.", "You may rely on it."
+]
+
+
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name="8-Ball answers."))
+  print('{0.user} is ready to reply.'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('?8ball'):
+    embed=discord.Embed(title="Here's what I think", description=(random.choice(ball)), color=0xff6961)
+    await message.channel.send(embed=embed)
+
+  if msg.startswith('?help'):
+    embed=discord.Embed(title="Help", description="Hi, I am a Magic 8-ball. Ask me a question with !8ball (question). I will reply with what I think will happen.", color=0xff6961)
+    await message.channel.send(embed=embed)
+
+keep_alive()
+client.run("ODMxNTIwNTMwMzIyNTU0ODgw.YHWb1w.Z0UQZkd1FkLBhpZkpJwZUtybDeA")
+
+# if you hack this bot, i wont like you anymore
