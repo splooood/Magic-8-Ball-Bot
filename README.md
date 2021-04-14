0d06cd2f2d56c88e74c296a61c54daddadce18cc
xSplode <>
2021-04-14 15:05:19 +0000

    Forked 8-Ball and made 9-Ball.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Magic 8-Ball
+# 9-Ball
 
-Simplistic Magic 8-Ball discord bot that replies with a randomized answer. It's very simple, even a 3-year-old can understand this. Maybe.
+A forked version of Magic 8-Ball. All it does is agree with people instead of giving a random answer.
 
 # How it works
 
-Works exactly like AnswerBot, but slightly different. Using the "random" tool, the command will get a randomized answer and reply to the user with said answer. Told you it was simple
+Just some modified code of 8-Ball.
 
 # Add this bot
 
-You can add the bot here: https://discord.com/api/oauth2/authorize?client_id=831520530322554880&permissions=322560&scope=bot
+You can add the bot here: 
+https://discord.com/oauth2/authorize?client_id=831907049881337866&permissions=8&scope=bot
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Magic 8 Ball Bot!
+# Magic 9 Ball Bot!
 # Made by Splode.
 
 import discord
@@ -10,6 +10,14 @@ client = client = discord.Client()
 
 ball = [
   "As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes, definitely.", "You may rely on it."
+]
+
+ballagree = [
+  "Yes", "Yes, very.", "As I see it, yes.", "It is certain.", "Without a doubt.", "Outlook good.", "Yes, definitely.", "You may rely on it.", "It is decidedly so.", "Most likely.", "Signs point to yes"
+]
+
+ballhatred = [
+  "Don't count on it.", " My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."
 ]
 
 
@@ -25,16 +33,16 @@ async def on_message(message):
 
   msg = message.content
 
-  if msg.startswith('?8ball'):
-    embed=discord.Embed(title="Here's what I think", description=(random.choice(ball)), color=0xff6961)
-    await message.channel.send(embed=embed)
 
   if msg.startswith('?help'):
-    embed=discord.Embed(title="Help", description="Hi, I am a Magic 8-ball. Ask me a question with !8ball (question). I will reply with what I think will happen.", color=0xff6961)
+    embed=discord.Embed(title="Help", description="Hi, I am 9-ball. Ask me a question with !9ball (question). I will agree with you always.", color=0xff6961)
     await message.channel.send(embed=embed)
 
+  if msg.startswith('?9ball'):
+    embed=discord.Embed(title="I agree.", description=(random.choice(ballagree)), color=0xff6961)
+    await message.channel.send(embed=embed)
+
+
 keep_alive()
-client.run("token")
+client.run("ODMxNTIwNTMwMzIyNTU0ODgw.YHWb1w.qpI-F0Ywhx8fqA_o3Drwszz_py0")
 
-
-#haha no token 4 u!!
